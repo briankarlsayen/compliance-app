5daff070a9f12298400e3cf360c61cb1dc3c8b9f
BrianSayen <blu3fire89@gmail.com>
2023-04-26 08:55:19 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Container, MuiThemeProvider } from '@material-ui/core'
-import oneplaceTheme from './theme/oneplace-theme'
-import CheckLists from './pages/Checklists'
-import Schedules from './pages/Schedules'
-import ScheduleForm from './pages/ScheduleForm'
+import { Container, MuiThemeProvider } from '@material-ui/core';
+import oneplaceTheme from './theme/oneplace-theme';
+import CheckLists from './pages/Checklists';
+import Schedules from './pages/Schedules';
+import ScheduleForm from './pages/ScheduleForm';
 
 export default function App() {
-    return (
-        // <KeycloakProvider>
-        <MuiThemeProvider theme={oneplaceTheme}>
-            <Container maxWidth={false}>
-                {/* <CheckLists /> */}
-                <Schedules />
-                {/* <ScheduleForm /> */}
-            </Container>
-        </MuiThemeProvider>
-        // </KeycloakProvider>
-    )
+  return (
+    // <KeycloakProvider>
+    <MuiThemeProvider theme={oneplaceTheme}>
+      <Container maxWidth={false}>
+        <CheckLists />
+        {/* <Schedules /> */}
+        {/* <ScheduleForm /> */}
+      </Container>
+    </MuiThemeProvider>
+    // </KeycloakProvider>
+  );
 }
